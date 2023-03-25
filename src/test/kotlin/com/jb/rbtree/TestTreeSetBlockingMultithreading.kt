bedaf5511cd66cb19706822b98a09c3c62d83a2c
Mikhail Yutman <myutman@gmail.com>
2023-03-25 16:17:36 +0300

    Added tests for TreeSet implementations. Created test class for multithreading test

---
+++ src/test/kotlin/com/jb/rbtree/TestTreeSetBlockingMultithreading.kt
@@ -0,0 +1,55 @@
+package com.jb.rbtree
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+import kotlinx.coroutines.runBlocking
+
+class TestTreeSetBlockingMultithreading {
+
+    @Test
+    fun testAdd() {
+        val set = RBTreeSetBlocking<Int>()
+        val elements = mutableSetOf<Int>()
+
+        val random = Random(339264239)
+
+        repeat(100000) {
+            val element = random.nextInt(from = -100000, until = 100000)
+            set.add(element)
+            elements.add(element)
+        }
+
+        Assertions.assertTrue(set.containsAll(elements))
+
+        repeat(100000) {
+            val element = random.nextInt(from = -100000, until = 100000)
+            Assertions.assertTrue(elements.contains(element) == set.contains(element))
+        }
+    }
+
+    @Test
+    fun testRemove() {
+        val set = RBTreeSetBlocking<Int>()
+        val elements = mutableSetOf<Int>()
+
+        set.addAll(-100000..100000)
+        elements.addAll(-100000..100000)
+
+        val random = Random(339264239)
+
+        repeat(100000) {
+            val element = random.nextInt(from = -100000, until = 100000)
+            set.remove(element)
+            elements.remove(element)
+        }
+
+        Assertions.assertTrue(set.containsAll(elements))
+
+        repeat(100000) {
+            val element = random.nextInt(from = -100000, until = 100000)
+            Assertions.assertTrue(elements.contains(element) == set.contains(element))
+        }
+    }
+}
